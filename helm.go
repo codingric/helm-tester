7bfbe0e7a4737a4796a42a34784b89ec4fec3687
Ricardo Salinas <admzwh5@bendigoadelaide.com.au>
2025-08-08 16:44:01 +1000

    fix(app): Better messaging

--- helm.go
+++ helm.go
@@ -316,12 +316,12 @@ func (h *HelmTester) AssertPodsUsingImage(t *testing.T, ns, labels, image string
 					if len(containers) > 0 {
 						for _, c := range containers {
 							if status.Name == c {
-								assert.Equal(tt, image, status.Image)
+								assert.Equal(tt, image, status.Image, "Image not expected")
 								break
 							}
 						}
 					} else {
-						assert.Equal(tt, image, status.Image)
+						assert.Equal(tt, image, status.Image, "Image not expected")
 					}
 				}
 			},
